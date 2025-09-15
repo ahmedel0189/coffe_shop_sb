8aa2a3284f945168fd63eb81592d9d352e93b03d
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-09-15 23:41:06 +0300

    Establish the Coffee Shop Page and basic Widgets

--- lib/layers/ui/screens/shop_screens.dart
+++ lib/layers/ui/screens/shop_screens.dart
@@ -1,12 +1,30 @@
+// import 'package:coffee_shop_sb/constants/my_colors.dart';
+import 'package:coffee_shop_sb/layers/data/models/coffee_shop_model.dart';
+import 'package:coffee_shop_sb/layers/ui/widgets/widgets_of_shop_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ShopScreens extends StatelessWidget {
   const ShopScreens({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('ShopScreens'),
+    return Consumer<CoffeeShopModel>(
+      builder: (context, value, child) {
+        return SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                WidgetsOfShopScreen()
+                    .buildOrderText(),
+                const SizedBox(height: 25),
+                WidgetsOfShopScreen().buildListOfTheCoffee(value),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
---
+++ lib/layers/ui/widgets/widgets_of_shop_screen.dart
@@ -0,0 +1,95 @@
+import 'package:coffee_shop_sb/constants/my_colors.dart';
+import 'package:coffee_shop_sb/layers/data/models/coffee_model.dart';
+import 'package:coffee_shop_sb/layers/data/models/coffee_shop_model.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class WidgetsOfShopScreen {
+  Widget buildOrderText() {
+    return Align(
+      alignment: Alignment.center,
+      child: Text(
+        'How Would You Like Your Coffee ?',
+        style: GoogleFonts.lobster(
+          fontSize: 24,
+          color: MyColors.myGnavColor,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget buildListOfTheCoffee(
+    CoffeeShopModel value,
+  ) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: value.coffeeShop.length,
+        itemBuilder: (context, index) {
+          CoffeeModel eachCoffee =
+              value.coffeeShop[index];
+          return buildCoffeeListTail(
+            eachCoffee: eachCoffee,
+            onPressed: () => addTocart(
+              context,
+              coffee: eachCoffee,
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  void addTocart(
+    BuildContext context, {
+    required CoffeeModel coffee,
+  }) {
+    Provider.of<CoffeeShopModel>(
+      context,
+      listen: false,
+    ).addItemToCart(coffee);
+  }
+
+  Widget buildCoffeeListTail({
+    required CoffeeModel eachCoffee,
+    required VoidCallback onPressed,
+  }) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      margin: EdgeInsets.all(15),
+      padding: EdgeInsets.symmetric(
+        horizontal: 10,
+        vertical: 25,
+      ),
+      child: ListTile(
+        title: Text(
+          eachCoffee.coffeeName,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        subtitle: Text(
+          "\$${eachCoffee.coffeePrice}",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: MyColors.myGnavColor,
+          ),
+        ),
+        leading: Image.asset(
+          eachCoffee.coffeeImage,
+        ),
+        trailing: IconButton(
+          icon: Icon(
+            Icons.add,
+            color: MyColors.myGnavColor,
+          ),
+          onPressed: onPressed,
+        ),
+      ),
+    );
+  }
+}
