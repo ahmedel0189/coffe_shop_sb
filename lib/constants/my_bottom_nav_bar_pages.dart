a7a805a11467b62ada4c2acf31e6f2bf7f5f86c3
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-09-15 14:40:58 +0300

    establish my bottom navbar list of screens

---
+++ lib/constants/my_bottom_nav_bar_pages.dart
@@ -0,0 +1,8 @@
+import 'package:coffee_shop_sb/layers/ui/screens/cart_screen.dart';
+import 'package:coffee_shop_sb/layers/ui/screens/shop_screens.dart';
+import 'package:flutter/material.dart';
+
+final List<Widget> gnavScreen = [
+  ShopScreens(),
+  CartScreen(),
+];
