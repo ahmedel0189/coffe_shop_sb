2fd4f5951788733ecd134aff4e4ff7e5c48dd229
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-09-15 23:38:55 +0300

    add the provider to the home page

--- lib/helper/approuter.dart
+++ lib/helper/approuter.dart
@@ -1,13 +1,18 @@
 import 'package:coffee_shop_sb/constants/my_string.dart';
+import 'package:coffee_shop_sb/layers/data/models/coffee_shop_model.dart';
 import 'package:coffee_shop_sb/layers/ui/screens/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class AppRouter {
   Route? generateroute(RouteSettings setting) {
     switch (setting.name) {
       case homePageRoutr:
         return MaterialPageRoute(
-          builder: (_) => HomePage(),
+          builder: (_) => ChangeNotifierProvider(
+            create: (context) => CoffeeShopModel(),
+            child: HomePage(),
+          ),
         );
     }
     return null;
