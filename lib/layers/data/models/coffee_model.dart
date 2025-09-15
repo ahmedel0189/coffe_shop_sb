13bd23acb74b5bf56a724a67063b0f5c5c26e6a9
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-09-15 15:45:28 +0300

    establish the coffee model

---
+++ lib/layers/data/models/coffee_model.dart
@@ -0,0 +1,10 @@
+class CoffeeModel {
+  final String coffeeName;
+  final String coffeeImage;
+  final int coffeePrice;
+  CoffeeModel({
+    required this.coffeeName,
+    required this.coffeeImage,
+    required this.coffeePrice,
+  });
+}
